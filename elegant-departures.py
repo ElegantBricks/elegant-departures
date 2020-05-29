a4fe4cf0e0d3302f2ab7839790d579f13f560ecc
Elegant Bricks <hello@elegantbricks.com>
2020-05-29 19:59:20 +0100

    Update elegant-departures.py

--- elegant-departures.py
+++ elegant-departures.py
@@ -1,6 +1,6 @@
 # Train Platform Data Script
 # Copyright (c) 2020 Oliver Hallifax, Elegant Bricks
- 
+
 # Modify values in these sections with care to customise your display 
 
 #Section 1
@@ -8,16 +8,18 @@
 #If set to "live" then the values in Section 2 are required to use the National Rail Enquiries real-time data lookup service.
 #You will need to register for an API key for this service.
 #If set to "fantasy" then the values in Section 3 are used to generate realistic looking train data with fake station names.
-mode = "live"
+mode = "fantasy"
 
 #Section 2
 #  For Realtime National Rail data:
 #  Key = API key obtained from their developer site
 #  URL = API endpoint (should not change normally or often)
 #  crs = CRS code for the station to show data for.
-key = "a238f3ea-8ed0-4a56-a9d9-9b6a28013a1f"
+#  dst = (OPTIONAL) CRS code for a destination station to filter the data for.  For all trains to Waterloo from Clapham Junction use 'crs = "CLJ"' and 'dst = "WAT"'.
+key = "a873c9af-3ef9-3e36-d3d7-9b4f39014a1f"
 url = 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb11.asmx'
 crs = "CLJ"
+dst = "BAL"
 
 #Section 3
 #  For fantasy timetable data:
@@ -77,6 +79,8 @@ except ImportError:
 i = 0
 rows = []
 lasttrain = mktime(localtime())
+destinationlength = 23
+platformlength = 6
 
 def get_eta(scheduled):
     #Function to randmly make some trains a little bit late.  Just like real life.
@@ -92,9 +96,9 @@ def get_eta(scheduled):
             hour = "0" + hour
         if (len(mins) == 1):
             mins = "0" + mins
-        
+
         return (etanumber)
-        
+
     else:
         return "On time"
 
@@ -129,28 +133,27 @@ def build_fantasy_data():
         currenttimenumber = currenttimenumber + random.choice(range(2,4))*60
         currenttimestring = ctime(currenttimenumber)
         eta = get_eta(currenttimenumber)
-     
+
         hour = currenttimestring[11:13]
         minute = currenttimestring[14:16]
-        
+
         #Set this on each run to get the highest value when the function finishes looping.
         global lasttrain
         lasttrain =  currenttimenumber
-        
+
         if len(str(eta)) > 7:
             etastring = ctime(eta)
             eta = etastring[11:13] + ":" + etastring[14:16]
-        
-        platform = str(random.choice(platforms))
-        destination = get_destination()
-        
+
+        platform = str(random.choice(platforms)).strip()
+        destination = get_destination().strip()
+
         if destination == "Hogwarts":
             platform = "9 3/4"
-        
-        newrow = hour + ":" + minute + " " + destination.ljust(23, ' ') + platform.ljust(6,' ') + str(eta)
+
+        newrow = hour + ":" + minute + " " + destination.ljust(destinationlength,' ') + platform.ljust(platformlength,' ') + str(eta)
         rows.append(newrow)
 
-    
 def update_fantasy_data():
     #Every period, check the list and remove any that are in the past.
     for row in rows:
@@ -158,7 +161,7 @@ def update_fantasy_data():
         currentminute = int(str(ctime())[14:16])
         currenthour = int(str(ctime())[11:13])
         currenttime = datetime.now()
-        
+
         #Check for a delayed time:
         if row.endswith("On time"):
             #Take the scheduled time
@@ -172,7 +175,7 @@ def update_fantasy_data():
             hours = traintime[0:2]
             mins = traintime[3:5]
             #print(traintime)
-        
+
         #Bodge the time using the hours and minutes on the screen    
         traintimestring = str(currenttime)[0:10] + " " + hours + ":" + mins + ":00.000000"
         traindatetime = datetime(int(traintimestring[0:4]),int(traintimestring[5:7]),int(traintimestring[8:10]),int(traintimestring[11:13]),int(traintimestring[14:16]))
@@ -180,11 +183,11 @@ def update_fantasy_data():
         diff = traindatetime - currenttime
         if diff.total_seconds() < -1000:
             traindatetime = traindatetime + timedelta(hours=24)
-        
+
         #Remove entries in the timetable which have already departed.
         if traindatetime < currenttime:
             del rows[rows.index(row)]
-            
+
     #We might have less than 3 on the board - let's build the list back up again.
     while len(rows) < 3:
         #take the current last train time in seconds and add a random number of minutes from 2 to 5 (expressed in seconds)
@@ -193,31 +196,33 @@ def update_fantasy_data():
 
         currenttimestring = ctime(lasttrain)
         eta = get_eta(lasttrain)
-     
+
         hour = currenttimestring[11:13]
         minute = currenttimestring[14:16]
-        
+
         if len(str(eta)) > 7:
             etastring = ctime(eta)
             eta = etastring[11:13] + ":" + etastring[14:16]
-  
-        platform = str(random.choice(platforms))
-        destination = get_destination()
-        
+
+        platform = str(random.choice(platforms)).strip()
+        destination = get_destination().strip()
+
         if destination == "Hogwarts":
             platform = "9 3/4"
-        
-        newrow = hour + ":" + minute + " " + destination.ljust(23, ' ') + platform.ljust(6,' ') + str(eta)
+
+        if len(destination) > destinationlength:
+            destination = destination[0:destinationlength]
+
+        newrow = hour + ":" + minute + " " + destination.ljust(destinationlength, ' ') + platform.ljust(platformlength,' ') + str(eta)
 
         rows.append(newrow)
 
-
 def fetch_nre_board():
-   
+
     try:      
         #Clear the whole board each time as data is coming live from NRE system.
         rows.clear()
-        
+
         headers = {'content-type': 'text/xml'}
         xml_payload = '''
             <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope"
@@ -232,41 +237,45 @@ def fetch_nre_board():
                 <ns0:GetDepartureBoardRequest>
                 <ns0:numRows>3</ns0:numRows>
                 <ns0:crs>{CRS}</ns0:crs>
-                <ns0:filterCrs></ns0:filterCrs>
+                <ns0:filterCrs>{DST}</ns0:filterCrs>
                 <ns0:filterType>to</ns0:filterType>
-                <ns0:timeOffset>1</ns0:timeOffset>
+                <ns0:timeOffset>2</ns0:timeOffset>
                 <ns0:timeWindow>120</ns0:timeWindow>
                 </ns0:GetDepartureBoardRequest>
             </SOAP-ENV:Body>
             </SOAP-ENV:Envelope>
         '''
 
-        payload = xml_payload.replace("{KEY}", key).replace("{CRS}", crs)
+        payload = xml_payload.replace("{KEY}", key).replace("{CRS}", crs).replace("{DST}", dst)
+
         response = requests.post(url, data=payload, headers=headers)
-        
+
         data = xmltodict.parse(response.content)
-    
+
         services = data["soap:Envelope"]["soap:Body"]["GetDepartureBoardResponse"]["GetStationBoardResult"]["lt7:trainServices"]["lt7:service"]
-        
+
         if type(services) is not list:
             services = [services]
 
         for service in services:
                 try:
-                    destination = service["lt5:destination"]["lt4:location"]["lt4:locationName"]
-                    destination = destination.ljust(22, ' ')
+                    destination = service["lt5:destination"]["lt4:location"]["lt4:locationName"].strip()
                 except Exception as e:
                     destination = ""
-            
+
                 try:
-                    platform = service["lt4:platform"]
+                    platform = service["lt4:platform"].strip()
                 except Exception as e:
                     platform = ""
-                
-                platform = platform.ljust(5, ' ')
-                rows.append(service["lt4:std"] + " " + destination + " " + platform + " " + service["lt4:etd"])
-              
-       
+
+                if len(destination) > destinationlength:
+                    destination = destination[0:destinationlength]
+
+                destination = destination.ljust(destinationlength, ' ')
+                platform = platform.ljust(platformlength, ' ')
+                newrow = service["lt4:std"] + " " + destination + platform + service["lt4:etd"]
+                rows.append(newrow)
+
     except Exception as e:
         logging.critical(e, exc_info=False) 
         rows.append("ERROR : Cannot get live data")
@@ -275,14 +284,13 @@ def fetch_nre_board():
 
 
 def show_data(device):
-    
-    pathname = os.path.dirname(sys.argv[0])        
+
+    pathname = os.path.dirname(os.path.abspath(__file__))
 
     # use custom fonts
     font1 = ImageFont.truetype(pathname + "/Cousine-Bold.ttf", 11)
     font2 = ImageFont.truetype(pathname + "/CourierPrime-Regular.ttf", 10)
     font3 = ImageFont.truetype(pathname + "/CourierPrime-Regular.ttf", 14)
-    
 
     with canvas(device) as draw:
             draw.text((0,0), 'DEPARTURES               Plat Expt', font=font1, fill="white")
@@ -294,14 +302,13 @@ def show_data(device):
             draw.text((100, 52), time.strftime("%H:%M:%S", time.localtime()), font=font3, fill="white")
  
 def show_startup(device):
-    
-    pathname = os.path.dirname(sys.argv[0])        
-	
+
+    pathname = os.path.dirname(os.path.abspath(__file__))
+
     # use custom fonts
     font1 = ImageFont.truetype(pathname + "/CourierPrime-Regular.ttf", 8)
     font2 = ImageFont.truetype(pathname + "/CourierPrime-Regular.ttf", 10)
     font3 = ImageFont.truetype(pathname + "/CourierPrime-Regular.ttf", 15)
-    
 
     with canvas(device) as draw:
             draw.text((0,0), 'The Elegant Departure Board', font=font3, fill="white")
@@ -316,13 +323,14 @@ def show_startup(device):
                     draw.text((0,46), 'and ' + str(latetrainpercent) + '% of trains run on time.', font=font1, fill="white")
                     print('and ' + str(latetrainpercent) + '% of trains run on time.')
                     print('Stations are : ' + str(towns))
-                    
             else:
                     draw.text((0,30), 'Running in live mode for station "' + crs +'"', font=font2, fill="white")
                     print('Running in live mode for station "' + crs +'"')
-            
+                    if len(dst)>0:
+                        draw.text((0,38), 'Filtered for trains to/via "' + dst + '"', font=font2, fill="white")
+                        print('Filtered for trains to/via "' + dst +'"')
     time.sleep(10)
-      
+
 def display_settings(args):
     """
     Display a short summary of the settings.
@@ -373,9 +381,9 @@ def get_device(actual_args=None):
     return device
 
 def main():
-    
+
     show_startup(device)
-    
+
     while True:
         global i
         if mode =="fantasy":
@@ -402,12 +410,11 @@ def main():
                 print("Error : in Live mode an API key is required from National Rail Enquiries.  You require a Darwin 'OpenLDBWS' free developer key.")
                 print("Please check at https://www.nationalrail.co.uk/100296.aspx for further information on this service.")
                 sys.exit()
-        
-        
+
             #Live data - call the API every 30 seconds
             if i==0:
                 fetch_nre_board()
-                
+
             #Update the screen
             show_data(device)
             i = i + 1
@@ -416,10 +423,7 @@ def main():
             time.sleep(1)
 
 if __name__ == "__main__":
-    
-    
 
-    
     try:
         device = get_device()
         main()
